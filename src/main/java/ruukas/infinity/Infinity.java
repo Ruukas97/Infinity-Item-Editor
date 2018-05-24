c95a7002bbd9816edb167d0e803e7446d8cfdc9f
Ruukas <lucas-olai@hotmail.com>
2018-05-24 02:52:22 +0200

    0.5.1 build

--- src/main/java/ruukas/infinity/Infinity.java
+++ src/main/java/ruukas/infinity/Infinity.java
@@ -22,7 +22,7 @@ public class Infinity
 {
     public static final String MODID = "infinity";
     public static final String NAME = "Infinity Item Editor";
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "0.5.1";
     
     public static Logger logger;
     
