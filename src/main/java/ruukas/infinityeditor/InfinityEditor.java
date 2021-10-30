c0e633fb8200841db1b34d8f56fdb56fc59b646c
Thieme <thieme@hoogernet.nl>
2021-10-30 14:41:46 +0200

    Updated version

--- src/main/java/ruukas/infinityeditor/InfinityEditor.java
+++ src/main/java/ruukas/infinityeditor/InfinityEditor.java
@@ -25,7 +25,7 @@ public class InfinityEditor
 {
     public static final String MODID = "infinityeditor";
     public static final String NAME = "Infinity Item Editor";
-    public static final String VERSION = "0.14.2";
+    public static final String VERSION = "0.14.3";
     
     public static Logger logger;
     public static File dataDir;
